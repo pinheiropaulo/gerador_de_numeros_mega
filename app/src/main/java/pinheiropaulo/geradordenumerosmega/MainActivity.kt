7c25f0e06206a84447e8c8d8bacf111b2b48c49f
Paulo Pinheiro <pauloprap57@gmail.com>
2022-08-24 17:10:11 -0300

    app finalizado

--- app/src/main/java/pinheiropaulo/geradordenumerosmega/MainActivity.kt
+++ app/src/main/java/pinheiropaulo/geradordenumerosmega/MainActivity.kt
@@ -1,11 +1,57 @@
 package pinheiropaulo.geradordenumerosmega
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        btnGenerate.setOnClickListener {
+            val text = editText.text.toString()
+
+            numberGenerator(text, txtResult)
+        }
     }
+
+    private fun numberGenerator(text: String, txtResult: TextView) {
+
+        if (text.isEmpty()) {
+            Toast.makeText(this, "Digite um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val qtd = text.toInt()
+
+        if (qtd < 6 || qtd > 15) {
+            Toast.makeText(this, "Digite um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+            }
+        }
+
+        txtResult.text = numbers.joinToString(" - ")
+    }
+
 }
